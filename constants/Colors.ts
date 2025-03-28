ba4c23182f6ee22226683fcb4f8a0463362d709f
RealmKnight <nick.schommer@gmail.com>
2025-03-27 22:37:19 -0500

    install supabase js and apply color changes

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,24 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = "#0a7ea4";
+const tintColorDark = "#fff";
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: "#11181C",
+    background: "#CACAACFF",
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: "#42627AFF",
+    tabIconDefault: "#234057FF",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: "#F1F148FF",
+    background: "#151718",
     tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    icon: "#6E98BAFF",
+    tabIconDefault: "#8EB1CEFF",
     tabIconSelected: tintColorDark,
   },
 };
